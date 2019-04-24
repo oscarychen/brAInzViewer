e673087ce6678af0ae5a21c6e5562c5901056be0
airoscar <oscar.y.chen@gmail.com>
2019-04-24 11:46:10 -0600

    added range slider for selecting display boundary of voxel values

--- Viewer/Viewer.py
+++ Viewer/Viewer.py
@@ -1,7 +1,9 @@
 from PyQt5.QtWidgets import (QApplication, QMainWindow, QMenu, QVBoxLayout, QSizePolicy, QMessageBox,
                              QWidget, QPushButton, QSlider, QHBoxLayout, QGroupBox, QRadioButton,
-                             QGridLayout, QLabel, QInputDialog, QFileDialog, QListWidget)
-from PyQt5.QtCore import Qt
+                             QGridLayout, QLabel, QInputDialog, QFileDialog, QListWidget, QFrame, QLayout)
+from PyQt5.QtCore import Qt, pyqtSlot, QMetaObject, QSize
+
+from PyQt5 import QtGui
 
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
@@ -11,7 +13,7 @@ import os
 import numpy as np
 
 DEBUG = False
-
+VOX_MAX_VAL = 2000
 
 class VolumeSelectView(QWidget):
     def __init__(self, parent=None):
@@ -46,6 +48,7 @@ class VolumeSelectView(QWidget):
         vbox.addWidget(self.volume_label)
         vbox.addWidget(self.slider)
         vbox.addWidget(self.triplane)
+        vbox.addWidget(DisplayRangeSelector(self.triplane))
 
         # Right-half: file list area
         self.file_list = QListWidget(self)
@@ -105,6 +108,100 @@ class VolumeSelectView(QWidget):
         self.triplane.coronalView.set_volume(value)
 
 
+class DisplayRangeSelector(QWidget):
+
+    def __init__(self, triplane):
+        super().__init__()
+        self.label = QLabel('Voxel Display Boundaries')
+        self.triplane = triplane
+        self.startSliderMaxValue = VOX_MAX_VAL
+        self.endSliderMaxValue = VOX_MAX_VAL
+        self.minSelected = VOX_MAX_VAL - self.startSliderMaxValue    # NOTE: the start slider is reversed in value
+        self.maxSelected = self.endSliderMaxValue
+
+        self.setupUi(self)
+
+    def setupUi(self, RangeSlider):
+        RangeSlider.setObjectName("RangeSlider")
+        RangeSlider.resize(1000, 65)
+        RangeSlider.setMaximumSize(QSize(16777215, 65))
+        self.RangeBarVLayout = QVBoxLayout(RangeSlider)
+        self.RangeBarVLayout.setContentsMargins(5, 0, 5, 0)
+        self.RangeBarVLayout.setSpacing(0)
+        self.RangeBarVLayout.setObjectName("RangeBarVLayout")
+
+        self.slidersFrame = QFrame(RangeSlider)
+        self.slidersFrame.setMaximumSize(QSize(16777215, 25))
+        self.slidersFrame.setFrameShape(QFrame.StyledPanel)
+        self.slidersFrame.setFrameShadow(QFrame.Raised)
+        self.slidersFrame.setObjectName("slidersFrame")
+        self.horizontalLayout = QHBoxLayout(self.slidersFrame)
+        self.horizontalLayout.setSizeConstraint(QLayout.SetMinimumSize)
+        self.horizontalLayout.setContentsMargins(5, 2, 5, 2)
+        self.horizontalLayout.setSpacing(0)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+
+        ## Start Slider Widget
+        self.startSlider = QSlider(self.slidersFrame)
+        self.startSlider.setMaximum(VOX_MAX_VAL)
+        self.startSlider.setMinimumSize(QSize(100, 5))
+        self.startSlider.setMaximumSize(QSize(16777215, 10))
+
+        font = QtGui.QFont()
+        font.setKerning(True)
+
+        self.startSlider.setFont(font)
+        self.startSlider.setAcceptDrops(False)
+        self.startSlider.setAutoFillBackground(False)
+        self.startSlider.setOrientation(Qt.Horizontal)
+        self.startSlider.setInvertedAppearance(True)
+        self.startSlider.setObjectName("startSlider")
+        self.startSlider.setValue(self.endSliderMaxValue)
+        self.startSlider.valueChanged.connect(self.handleStartSliderValueChange)
+        self.horizontalLayout.addWidget(self.startSlider)
+
+        ## End Slider Widget
+        self.endSlider = QSlider(self.slidersFrame)
+        self.endSlider.setMaximum(VOX_MAX_VAL)
+        self.endSlider.setMinimumSize(QSize(100, 5))
+        self.endSlider.setMaximumSize(QSize(16777215, 10))
+        self.endSlider.setTracking(True)
+        self.endSlider.setOrientation(Qt.Horizontal)
+        self.endSlider.setObjectName("endSlider")
+        self.endSlider.setValue(self.endSliderMaxValue)
+        self.endSlider.valueChanged.connect(self.handleEndSliderValueChange)
+
+        #self.endSlider.sliderReleased.connect(self.handleEndSliderValueChange)
+
+        self.horizontalLayout.addWidget(self.endSlider)
+        self.RangeBarVLayout.addWidget(self.label)
+        self.RangeBarVLayout.addWidget(self.slidersFrame)
+
+        #self.retranslateUi(RangeSlider)
+        QMetaObject.connectSlotsByName(RangeSlider)
+
+        self.show()
+
+    def updateLabel(self):
+        self.label.setText(f'Voxel Display Boundaries:({self.minSelected},{self.maxSelected})')
+
+    @pyqtSlot(int)
+    def handleStartSliderValueChange(self, value):
+        print(f'DEBUG: start slider = {value}')
+        self.startSlider.setValue(value)
+        self.minSelected = VOX_MAX_VAL - value  # NOTE: Start slider is reversed in value
+        self.triplane.voxDisplayUpdate(self.minSelected, self.maxSelected)
+        self.updateLabel()
+
+
+    @pyqtSlot(int)
+    def handleEndSliderValueChange(self, value):
+        print(f'DEBUG: end slider = {value}')
+        self.endSlider.setValue(value)
+        self.maxSelected = value
+        self.triplane.voxDisplayUpdate(self.minSelected, self.maxSelected)
+        self.updateLabel()
+
 class TriPlaneView(QWidget):
     def __init__(self, data, parent=None):
         if DEBUG: print("Debug: TriPlaneView.__init__")
@@ -113,8 +210,7 @@ class TriPlaneView(QWidget):
 
         self.data = data
         grid = QGridLayout()
-        self.axialView = PlaneView("Axial", self.data, 0,
-                                   self.data.shape[2] - 1)  # name, data, volume number, slice number
+        self.axialView = PlaneView("Axial", self.data, 0, self.data.shape[2] - 1)  # name, data, volume #, slice #
         self.sagittalView = PlaneView("Sagittal", self.data, 0, self.data.shape[0] - 1)
         self.coronalView = PlaneView("Coronal", self.data, 0, self.data.shape[1] - 1)
         grid.addWidget(self.axialView, 0, 0)
@@ -127,10 +223,19 @@ class TriPlaneView(QWidget):
         self.sagittalView.set_slider(self.data.shape[0] // 2)  # Initial slider position
         self.coronalView.set_slider(self.data.shape[1] // 2)  # Initial slider position
 
+    def voxDisplayUpdate(self, minValue, maxValue):
+        self.axialView.canvas.setMinVoxVal(minValue)
+        self.axialView.canvas.setMaxVoxVal(maxValue)
+        self.coronalView.canvas.setMinVoxVal(minValue)
+        self.coronalView.canvas.setMaxVoxVal(maxValue)
+        self.sagittalView.canvas.setMinVoxVal(minValue)
+        self.sagittalView.canvas.setMaxVoxVal(maxValue)
+        self.replot()
+
     def replot(self):
         if DEBUG: print('Debug: Update axial slider max to ' + str(self.data.shape[2]-1))
         self.axialView.setMaxSlider(self.data.shape[2] - 1)
-        self.axialView.set_slider(self.data.shape[2] // 2)  # Slider postion when loading a new file
+        self.axialView.set_slider(self.data.shape[2] // 2)  # Slider position when loading a new file
         self.axialView.replot()
 
         if DEBUG: print('Debug: Update sagittal slider max to ' + str(self.data.shape[0] - 1))
@@ -154,19 +259,9 @@ class TriPlaneView(QWidget):
         self.sagittalView.setData(data)
         self.coronalView.setData(data)
 
-class ViewControls(QWidget):
-    """Controls for each PlotCanvas class, vertical slider to adjust brightness, label selector, etc"""
+class LabelSelector(QWidget):
+    """label selector"""
     def __init__(self, maxVal, minVal):
-        self.maxVal = maxVal
-        self.slider = QSlider(Qt.Vertical)
-        self.slider.setFocusPolicy(Qt.StrongFocus)
-        self.slider.setTickPosition(QSlider.TicksBothSides)
-        self.slider.setTickInterval(100)
-        self.slider.setMinimum(0)
-        self.slider.setMaximum(maxVal)
-        self.slider.valueChanged.connect(self.updateBrightness)
-
-    def updateBrightness(self):
         pass
 
 class PlaneView(QWidget):
@@ -222,6 +317,7 @@ class PlaneView(QWidget):
         self.slice_num_label.setText(str(value))
 
 
+
 class PlotCanvas(FigureCanvas):
     def __init__(self, slicetype, data, volume, parent=None):
         if DEBUG: print("Debug: PlotCanvas.__init__")
@@ -250,6 +346,12 @@ class PlotCanvas(FigureCanvas):
     def setSliceIndex(self, value):
         self.cur_slice = value
         self.plot()
+
+    def setMinVoxVal(self, value):
+        self.minVoxVal = value
+
+    def setMaxVoxVal(self, value):
+        self.maxVoxVal = value
 
     def plot(self):
         self.ax.cla()
