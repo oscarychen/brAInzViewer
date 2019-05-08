417b09c82177036640dccdec7dcc32aa317dd882
Eiden Yoshida <eiden.yoshida@ucalgary.ca>
2019-05-07 20:38:41 -0600

    Added undersampler

--- CNN/TrainInRam.py
+++ CNN/TrainInRam.py
@@ -1,23 +1,30 @@
 import numpy as np
 import time
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D, ZeroPadding2D, BatchNormalization
+from keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D, BatchNormalization
 from keras.optimizers import Adam
 from keras.callbacks import ModelCheckpoint
 from tensorflow.keras.callbacks import TensorBoard
 
+
 #%%
-prefix = "C:/Users/Eiden/Desktop/BrainScanMotionDetection/CNN/DataArrays/4/"
+prefix = "C:/Users/Eiden/Desktop/BrainScanMotionDetection/CNN/DataArrays/under/"
 X_train = np.load(prefix + "dataxtrain.npy")
 X_test = np.load(prefix + "dataxtest.npy")
 y_train = np.load(prefix + "dataytrain.npy")
 y_test = np.load(prefix + "dataytest.npy")
+
+y_train = y_train[:,1]
+y_test = y_test[:,1]
+
 #%%
 
 # Design model
 layer_size = 32
-NAME = '{}'.format(int(time.time()))  # model name with timestamp
+batch_size = 32
+NAME = 'b{}_{}'.format(batch_size,int(time.time()))  # model name with timestamp
 model = Sequential()
+
 tensorboard = TensorBoard(log_dir='logs/{}'.format(NAME))
 checkpoint = ModelCheckpoint('weights/{}.h5'.format(NAME), monitor='val_loss', verbose=0, save_best_only=True, save_weights_only=False, mode='auto', period=1)
 callbacks = [tensorboard, checkpoint]
@@ -27,7 +34,7 @@ model.add(Conv2D(layer_size, (3,3), padding="same", activation="relu", input_sha
 model.add(BatchNormalization())
 model.add(MaxPooling2D(pool_size=(3,3)))
 
-for _ in range(5):
+for _ in range(4):
     model.add(Conv2D(layer_size, (3,3), padding="same", activation="relu"))
     model.add(BatchNormalization())
     model.add(Conv2D(layer_size, (3,3), padding="same", activation="relu"))
@@ -45,12 +52,24 @@ for _ in range(2):
     model.add(BatchNormalization())
     model.add(Dropout(0.6))
 
-model.add(Dense(2))
+model.add(Dense(1))
 model.add(Activation('sigmoid'))
-
-model.compile(loss='categorical_crossentropy',
+model.compile(loss='binary_crossentropy',
              optimizer=Adam(lr=0.008),
              metrics=['accuracy'])
 
+#model.add(Dense(2))
+#model.add(Activation('softmax'))
+
+#model.compile(loss='categorical_crossentropy',
+#             optimizer=Adam(lr=0.008),
+#             metrics=['accuracy'])
+
 # Train model on dataset
-model.fit(X_train, y_train,validation_data=(X_test, y_test), batch_size=128, epochs=3, callbacks=callbacks)
+model.fit(X_train, y_train,validation_data=(X_test, y_test), batch_size=32, epochs=100, callbacks = callbacks)
+
+#%%
+pred = model.predict(X_test)
+bad = [1 if prediction[1] >=0.5 else 0 for prediction in pred]
+print(sum(bad))
+print(sum(bad)/len(y_test))
