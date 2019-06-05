74f79eba754d954707ac05272f408ba41fbd29b9
Eiden Yoshida <eiden.yoshida@ucalgary.ca>
2019-06-05 10:59:26 -0600

    Moved code to same folder

--- Viewer/src/main/python/brAInzViewer/main.py
+++ Viewer/src/main/python/brAInzViewer/main.py
@@ -1,13 +1,12 @@
 from fbs_runtime.application_context import ApplicationContext, cached_property
 from PyQt5.QtWidgets import QMainWindow
-# from code.Controllers import Controller
 import sys
 import os
-sys.path.append(os.path.join(os.path.dirname(__file__), 'code'))
 from Controllers import Controller
 
+
 class AppContext(ApplicationContext):           # 1. Subclass ApplicationContext
-    
+
     def __init__(self, *args, **kwargs):
         super(AppContext, self).__init__(*args, **kwargs)
 
